4edbd9bd227e02f8b681959342905e69c0262f6d
pwilken <34198662+pwilken@users.noreply.github.com>
2018-01-02 20:28:29 +0100

    Fixed reversing of foraign keys (#15)
    The reversing of the most foraign keys was not working.

--- EFCore.FirebirdSql/Scaffolding/Internal/FbDatabaseModelFactory.cs
+++ EFCore.FirebirdSql/Scaffolding/Internal/FbDatabaseModelFactory.cs
@@ -119,17 +119,33 @@ GROUP BY I.RDB$INDEX_NAME, ISUNIQUE, I.RDB$RELATION_NAME, SG.RDB$FIELD_NAME";
 
         private readonly string _getConstraintsQuery = @"
 SELECT
-    CONST.RDB$CONSTRAINT_NAME,RELCONST.RDB$RELATION_NAME,REF.RDB$DELETE_RULE,
-    LIST(DISTINCT TRIM(IDX.RDB$FIELD_NAME)||'$'||IDX.RDB$FIELD_POSITION,',') COLUMN_NAME
+    FORAIGN.CONSTRAINT_NAME AS RDB$CONSTRAINT_NAME,
+    FORAIGN.RELATION_NAME AS RDB$RELATION_NAME,
+    FORAIGN.DELETE_RULE AS RDB$DELETE_RULE,
+    trim(trim(FORAIGN.column_name)||'$'||sg.rdb$field_name) AS COLUMN_NAME
 FROM
-    RDB$RELATION_CONSTRAINTS CONST
-    LEFT JOIN RDB$INDEX_SEGMENTS IDX ON CONST.RDB$INDEX_NAME = IDX.RDB$INDEX_NAME
-    LEFT JOIN RDB$REF_CONSTRAINTS REF ON CONST.RDB$CONSTRAINT_NAME = REF.RDB$CONSTRAINT_NAME
-    LEFT JOIN RDB$RELATION_CONSTRAINTS RELCONST ON REF.RDB$CONST_NAME_UQ = RELCONST.RDB$CONSTRAINT_NAME
-    LEFT JOIN RDB$INDEX_SEGMENTS IDXSEG ON RELCONST.RDB$INDEX_NAME = IDXSEG.RDB$INDEX_NAME
+    rdb$indices ix
+    left join rdb$index_segments sg on ix.rdb$index_name = sg.rdb$index_name
+    left join rdb$relation_constraints rc on rc.rdb$index_name = ix.rdb$index_name,
+        (
+        SELECT
+            detail_index_segments.RDB$FIELD_NAME AS column_name,
+            detail_relation_constraints.RDB$CONSTRAINT_NAME AS CONSTRAINT_NAME,
+            master_relation_constraints.RDB$RELATION_NAME AS RELATION_NAME,
+            rdb$ref_constraints.RDB$DELETE_RULE AS DELETE_RULE,
+            detail_relation_constraints.RDB$RELATION_NAME AS REFERENCE_TABLE_NAME
+        FROM
+            rdb$relation_constraints detail_relation_constraints
+            JOIN rdb$index_segments detail_index_segments ON detail_relation_constraints.rdb$index_name = detail_index_segments.rdb$index_name 
+            JOIN rdb$ref_constraints ON detail_relation_constraints.rdb$constraint_name = rdb$ref_constraints.rdb$constraint_name -- Master indeksas
+            JOIN rdb$relation_constraints master_relation_constraints ON rdb$ref_constraints.rdb$const_name_uq = master_relation_constraints.rdb$constraint_name
+            JOIN rdb$index_segments master_index_segments ON master_relation_constraints.rdb$index_name = master_index_segments.rdb$index_name 
+        WHERE
+            detail_relation_constraints.rdb$constraint_type = 'FOREIGN KEY' AND detail_relation_constraints.RDB$RELATION_NAME = '{0}'
+            
+    ) FORAIGN
 WHERE
-    CONST.RDB$CONSTRAINT_TYPE = 'FOREIGN KEY' AND CONST.RDB$RELATION_NAME = '{0}'
-GROUP BY CONST.RDB$CONSTRAINT_NAME, RELCONST.RDB$RELATION_NAME,REF.RDB$DELETE_RULE";
+    rc.rdb$constraint_type = 'PRIMARY KEY' AND FORAIGN.REFERENCE_TABLE_NAME = rc.rdb$relation_name";
         #endregion
 
         public FbDatabaseModelFactory(IDiagnosticsLogger<DbLoggerCategory.Scaffolding> loggerFactory)
@@ -350,6 +366,8 @@ GROUP BY CONST.RDB$CONSTRAINT_NAME, RELCONST.RDB$RELATION_NAME,REF.RDB$DELETE_RU
 
         private void GetConstraints()
         {
+            var exceptions = new Dictionary<DatabaseForeignKey, Exception>();
+
             foreach (var table in _tables)
             {
                 using (var command = new FbCommand(string.Format(_getConstraintsQuery, table.Key), _connection))
@@ -358,68 +376,49 @@ GROUP BY CONST.RDB$CONSTRAINT_NAME, RELCONST.RDB$RELATION_NAME,REF.RDB$DELETE_RU
                     {
                         while (rResult.Read())
                         {
-                            if (_tables.ContainsKey(table.Key))
+                            if (_tables.ContainsKey($"{ rResult.GetString(1).ToString().Trim() }"))
                             {
-                                var columnName = rResult["COLUMN_NAME"].ToString();
-                                var fkName = rResult["RDB$CONSTRAINT_NAME"].ToString().Trim();
-                                var onDelete = rResult["RDB$DELETE_RULE"].ToString().Trim();
-                                var principalTable = rResult["RDB$RELATION_NAME"].ToString().Trim();
-
-                                var foreignkey = new DatabaseForeignKey
+                                var fkInfo = new DatabaseForeignKey
                                 {
+                                    Name = rResult["RDB$CONSTRAINT_NAME"].ToString().Trim(),
+                                    OnDelete = ConvertToReferentialAction(rResult["RDB$DELETE_RULE"].ToString().Trim()),
                                     Table = table.Value,
-                                    Name = fkName,
-                                    OnDelete = ConvertToReferentialAction(onDelete),
-                                    PrincipalTable = _tables[principalTable]
+                                    PrincipalTable = _tables[rResult["RDB$RELATION_NAME"].ToString().Trim()]
                                 };
 
-                                var foreignkeyCols = columnName.Split(',');
-                                var columns = new string[foreignkeyCols.Length];
-                                foreach (var positionCol in foreignkeyCols)
+                                try
                                 {
-                                    var split = positionCol.Split('$');
-                                    columns[int.Parse(split[1])] = split[0].Trim();
-                                }
-
-                                foreach (var column in columns)
-                                {
-                                    var findColumn = table.Value.Columns
-                                        .FirstOrDefault(y => y.Name.Equals(column, StringComparison.OrdinalIgnoreCase));
-
-                                    if (findColumn != null
-                                        && !foreignkey.Columns.Where(i =>
-                                            i.Name.Equals(findColumn.Name, StringComparison.OrdinalIgnoreCase)).Any())
+                                    foreach (var pair in rResult.GetString(3).Split(','))
                                     {
-                                        foreignkey.Columns.Add(findColumn);
+                                        fkInfo.Columns.Add(table.Value.Columns.Single(y => y.Name == pair.Split('$')[0]));
+                                        fkInfo.PrincipalColumns.Add(fkInfo.PrincipalTable.Columns.Single(y => y.Name == pair.Split('$')[1]));
                                     }
+                                    table.Value.ForeignKeys.Add(fkInfo);
                                 }
-
-                                var foreignkeyColsSecundary = columnName.Split(',');
-                                var columnsSecundary = new string[foreignkeyColsSecundary.Length];
-
-                                foreach (var positionCol in foreignkeyColsSecundary)
+                                catch (Exception e)
                                 {
-                                    var split = positionCol.Split('$');
-                                    columnsSecundary[int.Parse(split[1])] = split[0];
+                                    exceptions.Add(fkInfo, e);
                                 }
-
-                                foreach (var column in columnsSecundary)
-                                {
-                                    var findColumn = table.Value.Columns
-                                        .FirstOrDefault(y => y.Name.Equals(column.Trim(), StringComparison.OrdinalIgnoreCase));
-
-                                    if (findColumn != null
-                                        && !foreignkey.PrincipalColumns.Where(i => i.Name == findColumn.Name).Any())
-                                    {
-                                        foreignkey.PrincipalColumns.Add(findColumn);
-                                    }
-                                }
-
-                                table.Value.ForeignKeys.Add(foreignkey); 
+                            }
+                            else
+                            {
+                                Logger.Logger.LogWarning($"Referenced table `{ rResult.GetString(1).ToString().Trim() }` is not in dictionary.");
                             }
                         }
                     }
                 }
+            }
+
+            if (exceptions.Count > 0)
+            {
+                Logger.Logger.LogWarning($"Unable to reverse the following foraign keys ({exceptions.Count})");
+
+                foreach (var exception in exceptions)
+                {
+                    Logger.Logger.LogWarning($"Table: {exception.Key.Table.Name} Referenced-Table: " +
+                                             $"{exception.Key.PrincipalTable.Name} Constraint-Name: {exception.Key.Name}");
+                }
+                Logger.Logger.LogWarning($"---------------------------------------------------------");
             }
         }
 
