fd0f9441a5b41daca33fb1817f2d732b84bb0c61
ralmsdeveloper <ralms@ralms.net>
2018-02-04 12:11:41 -0200

    Cleanup!

--- EFCore.FirebirdSql.FunctionalTests/F1FirebirdFixture.cs
+++ EFCore.FirebirdSql.FunctionalTests/F1FirebirdFixture.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+ *          Copyright (c) 2017 Rafael Almeida (ralms@ralms.net)
+ *
+ *                    EntityFrameworkCore.FirebirdSql
+ *
+ * THIS MATERIAL IS PROVIDED AS IS, WITH ABSOLUTELY NO WARRANTY EXPRESSED
+ * OR IMPLIED.  ANY USE IS AT YOUR OWN RISK.
+ *
+ * Permission is hereby granted to use or copy this program
+ * for any purpose,  provided the above notices are retained on all copies.
+ * Permission to modify the code and to distribute modified code is granted,
+ * provided the above notices are retained, and a notice that the code was
+ * modified is included with the above copyright notice.
+ *
+ */
+
+using System;
 using EFCore.FirebirdSql.FunctionalTests;
 using Microsoft.EntityFrameworkCore.TestModels.ConcurrencyModel;
 using Microsoft.Extensions.DependencyInjection;
