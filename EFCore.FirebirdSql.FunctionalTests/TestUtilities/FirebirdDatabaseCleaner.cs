beedca29b133ad12058a4d7782382b9cb9aa1293
ralmsdeveloper <ralms@ralms.net>
2018-02-04 17:40:15 -0200

    Code Cleanup :)

--- EFCore.FirebirdSql.FunctionalTests/TestUtilities/FirebirdDatabaseCleaner.cs
+++ EFCore.FirebirdSql.FunctionalTests/TestUtilities/FirebirdDatabaseCleaner.cs
@@ -42,62 +42,21 @@ namespace EFCore.FirebirdSql.FunctionalTests.TestUtilities
             => false;
 
         protected override string BuildCustomSql(DatabaseModel databaseModel)
-            => @"
-DECLARE @name VARCHAR(MAX) = '__dummy__', @SQL VARCHAR(MAX);
-
-WHILE @name IS NOT NULL
-BEGIN
-    SELECT @name =
-    (SELECT TOP 1 QUOTENAME(s.[name]) + '.' + QUOTENAME(o.[name])
-     FROM sysobjects o
-     INNER JOIN sys.views v ON o.id = v.object_id
-     INNER JOIN sys.schemas s ON s.schema_id = v.schema_id
-     WHERE (s.name = 'dbo' OR s.principal_id <> s.schema_id) AND o.[type] = 'V' AND o.category = 0 AND o.[name] NOT IN
-     (
-        SELECT referenced_entity_name
-        FROM sys.sql_expression_dependencies AS sed
-        INNER JOIN sys.objects AS o ON sed.referencing_id = o.object_id
-     )
-     ORDER BY v.[name])
-
-    SELECT @SQL = 'DROP VIEW ' + @name
-    EXEC (@SQL)
-END";
+            => string.Empty;
 
         protected override string BuildCustomEndingSql(DatabaseModel databaseModel)
-            => @"
-DECLARE @SQL VARCHAR(MAX) = '';
-SELECT @SQL = @SQL + 'DROP FUNCTION ' + QUOTENAME(ROUTINE_SCHEMA) + '.' + QUOTENAME(ROUTINE_NAME) + ';'
-  FROM [INFORMATION_SCHEMA].[ROUTINES] WHERE ROUTINE_TYPE = 'FUNCTION' AND ROUTINE_BODY = 'SQL';
-EXEC (@SQL);
-
-SET @SQL ='';
-SELECT @SQL = @SQL + 'DROP AGGREGATE ' + QUOTENAME(ROUTINE_SCHEMA) + '.' + QUOTENAME(ROUTINE_NAME) + ';'
-  FROM [INFORMATION_SCHEMA].[ROUTINES] WHERE ROUTINE_TYPE = 'FUNCTION' AND ROUTINE_BODY = 'EXTERNAL';
-EXEC (@SQL);
-
-SET @SQL ='';
-SELECT @SQL = @SQL + 'DROP PROC ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name) + ';' FROM sys.procedures;
-EXEC (@SQL);
-
-SET @SQL ='';
-SELECT @SQL = @SQL + 'DROP TYPE ' + QUOTENAME(schema_name(schema_id)) + '.' + QUOTENAME(name) + ';' FROM sys.types WHERE is_user_defined = 1;
-EXEC (@SQL);
-
-SET @SQL ='';
-SELECT @SQL = @SQL + 'DROP SCHEMA ' + QUOTENAME(name) + ';' FROM sys.schemas WHERE principal_id <> schema_id;
-EXEC (@SQL);";
+            => string.Empty;
 
         protected override DropTableOperation Drop(DatabaseTable table)
-            => AddMemoryOptimizedAnnotation(base.Drop(table), table);
+            => AdDOptimizedAnnotation(base.Drop(table), table);
 
         protected override DropForeignKeyOperation Drop(DatabaseForeignKey foreignKey)
-            => AddMemoryOptimizedAnnotation(base.Drop(foreignKey), foreignKey.Table);
+            => AdDOptimizedAnnotation(base.Drop(foreignKey), foreignKey.Table);
 
         protected override DropIndexOperation Drop(DatabaseIndex index)
-            => AddMemoryOptimizedAnnotation(base.Drop(index), index.Table);
+            => AdDOptimizedAnnotation(base.Drop(index), index.Table);
 
-        private static TOperation AddMemoryOptimizedAnnotation<TOperation>(TOperation operation, DatabaseTable table)
+        private static TOperation AdDOptimizedAnnotation<TOperation>(TOperation operation, DatabaseTable table)
             where TOperation : MigrationOperation => operation;
     }
 }
