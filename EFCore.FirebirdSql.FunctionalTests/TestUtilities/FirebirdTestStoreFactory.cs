5399ea8be13b9d3aafa8a128032e0c5ce50ef86a
jakub.pawlowski <jakub.pawlowski@live.com>
2018-06-21 07:39:50 +0200

    fix FbDesignTimeServices for use with ef context scaffolding
    * use same namespace in FbDesignTimeServices  as everywhere else
    * add FbOptions to serviceCollection in FbDesignTimeServices

--- EFCore.FirebirdSql.FunctionalTests/TestUtilities/FirebirdTestStoreFactory.cs
+++ EFCore.FirebirdSql.FunctionalTests/TestUtilities/FirebirdTestStoreFactory.cs
@@ -15,10 +15,10 @@
  */
 
 using System.Diagnostics;
+using EntityFrameworkCore.FirebirdSql.Design.Internal;
 using EntityFrameworkCore.FirebirdSql.Migrations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.EntityFrameworkCore.Firebird.Design.Internal;
 using Microsoft.EntityFrameworkCore.Internal;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
--- EFCore.FirebirdSql/Design/Internal/FbDesignTimeServices.cs
+++ EFCore.FirebirdSql/Design/Internal/FbDesignTimeServices.cs
@@ -15,6 +15,8 @@
  */
 
 using EntityFrameworkCore.FirebirdSql.Design.Internal;
+using EntityFrameworkCore.FirebirdSql.Infrastructure.Internal;
+using EntityFrameworkCore.FirebirdSql.Internal;
 using EntityFrameworkCore.FirebirdSql.Scaffolding.Internal;
 using EntityFrameworkCore.FirebirdSql.Storage.Internal.Mapping;
 using Microsoft.EntityFrameworkCore.Design;
@@ -22,7 +24,7 @@ using Microsoft.EntityFrameworkCore.Scaffolding;
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Microsoft.EntityFrameworkCore.Firebird.Design.Internal
+namespace EntityFrameworkCore.FirebirdSql.Design.Internal
 {
     public class FbDesignTimeServices : IDesignTimeServices
     {
@@ -30,8 +32,9 @@ namespace Microsoft.EntityFrameworkCore.Firebird.Design.Internal
             => serviceCollection
                 .AddSingleton<IRelationalTypeMappingSource, FbTypeMappingSource>()
                 .AddSingleton<IDatabaseModelFactory, FbDatabaseModelFactory>()
-                .AddSingleton<ProviderCodeGenerator, FbScaffoldingCodeGenerator>()
-                .AddSingleton<IAnnotationCodeGenerator, FbAnnotationCodeGenerator>();
+                .AddSingleton<IProviderConfigurationCodeGenerator, FbScaffoldingCodeGenerator>()
+                .AddSingleton<IAnnotationCodeGenerator, FbAnnotationCodeGenerator>()
+                .AddSingleton<IFbOptions, FbOptions>();
     }
 }
 
